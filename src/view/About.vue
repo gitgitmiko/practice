ff5f3ee265823d20a82d78b3282bd37a96c4114f
Sigit Jatmiko <sjatmiko30@gmail.com>
2022-01-21 15:26:46 +0700

    update

---
+++ src/view/About.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="about">
+        <p>About</p>
+    </div>  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/view/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="home">
+        <p>hello world</p>
+    </div>  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
